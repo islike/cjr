3dd871b4de39036e723ddb3ca33d8534a84c2db9
du <649237053@qq.com>
2019-01-07 15:31:48 +0800

    du

--- src/pages/about/about.vue
+++ src/pages/about/about.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <holder></holder>
-    {{this.msg}}
+    <div class="about">
+        <div class="left">
+            <div v-for="(item ,index) in feather" v-html="item" :class=" current == index ? 'redright' : ''" @click="current = index">
+            </div>
+        </div>
+        <div class="right">
+          <div class=" header" v-html="feather[current]"></div>
+          <div class="content">
+            很棒棒团
+          </div>
+        </div>
+    </div>
     <foot></foot>
   </div>
 </template>
@@ -16,11 +27,53 @@ export default {
   },
   data () {
     return {
-      msg: '当前是关于页面'
+     feather:['关于我们','加入我们','网站声明'],
+     current:0,
     }
   }
 }
 </script>
-<style scoped>
+<style lang=less scoped>
+     
+    .about{
+      background:#ccc;
+      width:100%;
+      height:500px;
+      display:flex;
+      justify-content:space-between;
+      padding:0 20px;
 
+       .left{
+         width:100px;
+         height:100px;
+         font-size:14px;
+          
+          .redright{
+            border-left:2px solid red;
+          }
+           
+           div{
+             padding:5px 0px 5px 10px ;
+             background:white;
+             margin-top:1px;
+           }
+       }
+
+       .right{
+         flex:1;
+         background:white;
+         margin:0px 10px;
+         font-size:14px;
+
+
+         .header{
+           padding:5px 10px;
+           border-bottom:1px solid #ccc;
+           
+         }
+         .content{
+           padding-left:10px;
+         }
+       }
+    }
 </style>
--- src/pages/feedback/feedback.vue
+++ src/pages/feedback/feedback.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <holder></holder>
-   <!--  {{this.msg}} -->
+    <div class="feedback">
+         <div class="feedstyle common">
+           <div>反馈类型:</div>
+           <div>
+             <select>
+               <option>1</option>
+               <option>1</option>
+               <option>1</option>
+               <option>1</option>
+             </select>
+           </div>
+         </div>
+         <div class="feedcontent common">
+           <div>反馈内容:</div>
+           <div>
+             <textarea cols=80 rows=20 placeholder="写下反馈内容" v-model="content"></textarea>
+           </div>
+         </div>
+         <div class="contact common">
+           <div>联系方式：</div>
+           <input type="number" maxSize=11 placeholder="请输入联系方式" v-model="meassage">
+         </div>
+    </div>
     <foot></foot>
   </div>
 </template>
@@ -16,11 +38,39 @@ export default {
   },
   data () {
     return {
-      msg: '当前是反馈页面'
+      meassage: null,
+      content:null,
     }
   }
 }
 </script>
-<style>
+<style lang="less" scoped>
+   .feedback{
+     width:100%;
+     background:#ccc;
+     height:500px;
+     padding-left:20px;
 
+
+     .common{
+       display:flex;
+       margin-top:15px;
+ 
+           div:nth-child(2){
+             margin-left:10px;
+            
+             border:1px solid #ccc;
+
+             select{
+               width:150px;
+               
+             }
+
+             textarea{
+               border-radius:3px;
+             }
+           }
+           
+     }
+   }
 </style>
